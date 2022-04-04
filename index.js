546e2f6d0dc5e86e750d4a24bc613478aea1f817
Frank Ker <chingiap.ker@hilti.com>
2022-04-04 13:50:36 +0200

    add dockerfile and gitlab-ci.yml
    add dockerfile and gitlab-ci.yml
    
    housekeeping
    
    test pipeline
    
    add dockerfile and gitlab-ci.yml
    
    add dockerfile and gitlab-ci.yml
    
    add dockerfile and gitlab-ci.yml

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const { ApolloServer, gql } = require('apollo-server');
 const {ApolloGateway} = require('@apollo/gateway')
+const process = require('process');
 
 const gateway = new ApolloGateway({
     serviceList: [
-        { name: 'profile', url: process.env.CCMS_GRAPHQL_ENDPOINT },
-        { name: 'timeAndAttendance', url: process.env.WFM_TA_GRAPHQL_ENDPOINT },
+        // { name: 'profile', url: process.env.CCMS_GRAPHQL_ENDPOINT },
+        { name: 'timeAndAttendance', url: process.env.WFM_TNA_GRAPHQL_ENDPOINT },
     ]
 });
 
 const server = new ApolloServer({ gateway, subscriptions:false, tracing:true });
-server.listen();
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+    console.log(
+      `Try your health check at: ${url}.well-known/apollo/server-health`,
+    );
+  });
